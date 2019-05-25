1be99ecc18913bf0a98c9d62bcee90a36fcdad4c
Max Base <MaxBaseCode@Gmail.Com>
2019-05-25 20:26:22 +0430

    Update 1.php

--- tests/php/1.php
+++ tests/php/1.php
@@ -1,1 +1,13 @@
-
+<?php
+/* dfgdgdf gdfg */
+for ($v = 7;$v <= 100 / 10;$v++) {
+$b = $v;
+$x = [];
+for ($i = 1;$i <= $v;$i++) {
+$x[] = $i;
+}
+for ($k = 3;$k <= ((floor($v - 1) / 2) + 1);$k++) {
+$r = $k;
+solve($x, $v, $b, $k, $r);
+}
+}
