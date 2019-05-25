d6ab7640dd961fc4579ec32cb2bd18d9cb84abc6
Max Base <MaxBaseCode@Gmail.Com>
2019-05-25 13:06:06 +0430

    Update Parser.php

--- Parser.php
+++ Parser.php
@@ -1,4 +1,14 @@
 <?php
+/**
+*
+* @Name : PHPBeautifier/Parser.php
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-05-25
+* @Released under : https://github.com/BaseMax/PHPBeautifier/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/PHPBeautifier
+*
+**/
 // startsWith
 function starts($input, $need) {
 	$length=strlen($need);
@@ -15,10 +25,15 @@ function ends($input, $need) {
 $filename="test.php";
 $input=file_get_contents($filename);
 $_open_tag=false;
+
+$_open_string=false;
 $_open_dblstring=false;
 $_open_sngstring=false;
+
+$_open_comment="";
 $_open_sngcomment=false;
 $_open_mltcomment=false;
+
 $_index=0;
 $_length=mb_strlen($input);
 $result="";
@@ -63,19 +78,27 @@ for($_index;$_index<$_length;$_index++) {
 	else {
 		if($_open_sngcomment === false && $_open_mltcomment === false && $_open_sngstring === false && $_open_dblstring === false) {
 			if(starts($_chars, " ")) { }
+
 			else if(starts($_chars, "\t")) { }
+
 			// else if(starts($_chars, "\n")) { }
+
 			else if(starts($_chars, "//")) {
 				$_open_sngcomment=true;
 				$_index+=1;
+				$_open_comment="";
 			}
+
 			else if(starts($_chars, "/*")) {
 				$_open_mltcomment=true;
 				$_index+=1;
+				$_open_comment="";
 			}
+
 			else if(starts($_chars, "{")) {
 				$result.=" {";
 			}
+
 			else {
 				$result.=$_char;
 			}
@@ -83,16 +106,46 @@ for($_index;$_index<$_length;$_index++) {
 		else if($_open_sngcomment === true) {
 			if(starts($_chars, "\n")) {
 				$_open_sngcomment=false;
+				$_open_comment=trim($_open_comment);
+				if($_open_comment != "") {
+					$result.="// ";
+					$result.=$_open_comment;
+					$result.="\n";
+				}
+			}
+			else {
+				$_open_comment.=$_char;
 			}
 		}
 		else if($_open_mltcomment === true) {
+			$_do=false;
 			if(starts($_chars, "*/\n")) {
+				$_do=true;
 				$_index+=2;
-				$_open_mltcomment=false;
 			}
 			else if(starts($_chars, "*/")) {
+				$_do=true;
 				$_index+=1;
+			}
+			else {
+				$_open_comment.=$_char;
+			}
+			if($_do === true) {
 				$_open_mltcomment=false;
+				$_open_comment=trim($_open_comment);
+				if($_open_comment != "") {
+					$_comment_lines=substr_count($_open_comment, "\n");
+					if($_comment_lines > 0) {
+						$result.="/*\n";
+						$result.=$_open_comment;
+						$result.="*/\n";
+					}
+					else {
+						$result.="/* ";
+						$result.=$_open_comment;
+						$result.=" */\n";
+					}
+				}
 			}
 		}
 		else if($_open_sngstring === true) {
