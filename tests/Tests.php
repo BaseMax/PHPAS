0ff902de55b4a3f2572154459ae6d46df5413c4f
Max Base <MaxBaseCode@Gmail.Com>
2019-05-29 21:56:13 +0430

    Update and rename Tests.php to tests/Tests.php

--- tests/Tests.php
+++ tests/Tests.php
@@ -1,11 +1,11 @@
 <?php
 include "PHPAS.php";
-if($files = opendir('./tests/php/')) {
+if($files = opendir('./php/')) {
 	$index=1;
 	while(false !== ($file = readdir($files))) {
 		if($file != "." && $file != "..") {
 			$AS=new PHPAutoStyle($file);
-			$output_filename="tests/output/". $file;
+			$output_filename="output/". $file;
 			if(file_exists($output_filename)) {
 				$output=file_get_contents($output_filename);
 				// $output=trim($output);
