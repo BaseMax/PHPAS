f3bb310be10ec8545da4cb68d78eb2255ea22c5b
Max Base <MaxBaseCode@Gmail.Com>
2020-12-02 17:27:14 +0330

    Improve Tests.php / #5

--- tests/Tests.php
+++ tests/Tests.php
@@ -2,17 +2,20 @@
 // https://github.com/BaseMax/PHPAS
 include "../PHPAS.php";
 
-if($files = opendir('./php/')) {
+$dir = './php/';
+if($files = opendir($dir)) {
 	$index=1;
 	while(false !== ($file = readdir($files))) {
 		if($file != "." && $file != "..") {
-			$AS=new PHPAutoStyle($file);
+			$AS=new PHPAutoStyle();
+			$AS->loadFile($dir . $file);
 			$output_filename="output/". $file;
 			if(file_exists($output_filename)) {
 				$output=file_get_contents($output_filename);
 				// $output=trim($output);
 				print "-> $index / $file => ";
-				// Maybe it displays false, because GitHub change the indent, tab style...
+				file_put_contents($output_filename.".tmp", $AS->result);
+				// if(strcmp($AS->result, $output) === 0) {
 				if($AS->result == $output) {
 					print "TRUE";
 				}
